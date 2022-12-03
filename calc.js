b776e27e73bb93d63238a3eaaba7f1a86a8f969e
Gamolly <gamollygamolly@gmail.com>
2022-12-03 18:43:19 -0500

    Add basic functions and some thoughts on logic

---
+++ calc.js
@@ -0,0 +1,30 @@
+function add(a,b){
+    return a+b;
+}
+
+function subtrak(a,b){
+    return a-b;
+}
+
+function mult(a,b){
+    return a*b;
+}
+
+function divide(a,b){
+    return a/b;
+}
+
+
+// display = 123
+// >> operator pressed 
+//   >> let a = (display).toNumber()
+//   >> add rest of numbers to display 
+// option 1>> equals pressed 
+//  >> let b = display[from the end up to length(a)+1]
+//  >> display = operation output 
+// option 2>> another operator pressed 
+//  >>let b = ...
+//  >>result of first operator (a,b)
+//  >> a = result 
+//  >> add result of numbers to display 
+//  >> equals pressed? repeat as above
