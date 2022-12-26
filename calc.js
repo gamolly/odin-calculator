7800cc0431a972e995a24d913ed52ccaceb70c54
Gamolly <gamollygamolly@gmail.com>
2022-12-26 09:45:53 -0500

    Add logic to string operations
    The logic is to be able to keep using operations without having
    to press enter or '=' each time.

--- calc.js
+++ calc.js
@@ -2,16 +2,16 @@ const validKeys = '1234567890.*/+-';
 const validNums = '1234567890.';
 const validOps = '*/+-';
 const opDict = {
-    "*" : mult,
-    "/" : divide,
-    "+" : add,
-    "-" : subtrak,
+    "*": mult,
+    "/": divide,
+    "+": add,
+    "-": subtrak,
 }
 const display = document.getElementById('display');
 let displayVal, firstNum, secondNum, result;
 firstNum = secondNum = result = 0;
 let funk = add;
-let operatorPressed = false;
+let operatorPressed = 0;
 
 function add(a, b) {
     return a + b;
@@ -33,15 +33,22 @@ function operate(a, b, fun) {
     return fun(a, b);
 }
 
-function updateDisplay(e) {
+function updateDisplay(e) { //I should  probably split this into functions for clarity
 
     let val;
-
     console.log(firstNum);
 
     if (e instanceof KeyboardEvent) {
         val = e.key;
     } else val = e.target.textContent;
+    
+
+    if (val == 'clr'){
+        console.log('inside clear')
+        operatorPressed = firstNum = secondNum = result = 0;
+        display.textContent = '0';
+        
+    }
 
     if (validKeys.includes(val)) {
         // clear the display to update with user input
@@ -52,29 +59,51 @@ function updateDisplay(e) {
         display.textContent += val;
 
         //if operator is pressed save the value before the operator into the first number 
-        if (validOps.includes(val)){
-            operatorPressed = true;
-            // firstNum = Number(display.textContent.slice(0,display.textContent.length-1));
-            console.log(firstNum);
-            funk = opDict[val];
-            console.log(funk);
+        if (validOps.includes(val)) {
+
+            operatorPressed += 1;
+            console.log(`operatorPress = ${operatorPressed}`)
+            if (operatorPressed == 1){
+                console.log(`i'm in the if`)
+                firstNum = display.textContent.slice(0, display.textContent.length - 1);
+                console.log(firstNum);
+                funk = opDict[val];
+                console.log(funk);
+            }
+            else{
+                
+                console.log(`i'm in else`)
+                secondNum = display.textContent.slice(firstNum.length + 1, display.textContent.length-1);
+                console.log(`firstNum = ${firstNum}, secondNum = ${secondNum}, funk = ${funk}`);
+                //if no operator pressed (i.e., only numbers inputted in display), asking for the result returns the display content directly
+                //otherwise, operate
+                result = operatorPressed ? operate(Number(firstNum), Number(secondNum), funk) : display.textContent;
+                firstNum = display.textContent = result.toString();
+                console.log(`firstNum is ${firstNum} and its length = ${firstNum.length}`)
+                operatorPressed = 1;
+                display.textContent += val;
+                funk = opDict[val];
+                
+            }
         }
     }
-    if (val == '=' | val == "Enter"){
+    
+    if (val == '=' | val == "Enter") {
+        secondNum = display.textContent.slice(firstNum.length + 1, display.textContent.length);
         console.log(`firstNum = ${firstNum}, secondNum = ${secondNum}, funk = ${funk}`);
         //if no operator pressed (i.e., only numbers inputted in display), asking for the result returns the display content directly
         //otherwise, operate
-        result = operatorPressed ? operate(firstNum, secondNum, funk) : display.textContent; 
+        result = operatorPressed ? operate(Number(firstNum), Number(secondNum), funk) : display.textContent;
         display.textContent = result;
-        operatorPressed = false;
-        console.log(result);
+        operatorPressed = 0;
     }
-    /* if first thing on display is operator + or - , should work fine
+    /*TO ADD if first thing on display is operator + or - , should work fine
     if first thing on display is * or / then ignore*/
 
     if (val == 'Backspace' & display.textContent.length != 0) {
         display.textContent = display.textContent.slice(0, -1);
     }
+
 }
 
 window.addEventListener('keydown', updateDisplay);
@@ -89,7 +118,7 @@ calcButtons.forEach(button => button.addEventListener('click', updateDisplay))
 // let dispVal = display.textContent ( = '555+12*135-235+25/2-1325' )
 // start loop to find indices of '*'
 // index('*') = n 
-  // start to loops to move forward and backwards from n 
+  // start to loops to move forward and backwards from n
   // so, first loop dispVal [n-i] and backwards stop when validOps.include(dispVal[n-i])
      // number of steps taken until stop = stepBack;
   // second loop dispVal [n+i] and forwards and stop when validOps.include(dispVal[n-i])
